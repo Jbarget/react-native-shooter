81b78972ee813e547f5649b214c2789beed1614e
jbarget <dear.justen@gmail.com>
2018-10-30 13:27:28 +0100

    adding permission request for camera and audio

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Camera, Permissions } from "expo";
 
 export default class App extends React.Component {
+  state = {
+    hasAudioPermissions: null,
+    hasCameraPermissions: null
+  };
+
+  async componentWillMount() {
+    const { hasAudioPermissions, hasCameraPermissions } = this.state;
+  }
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: "transparent",
+                flexDirection: "row"
+              }}
+            />
+          </Camera>
+        </View>
+      );
+    }
   }
 }
 
